e860c029e25af03e2920e7a403396651885be9c5
Dzakwan Al iman <dzakwan112016@gmail.com>
2025-07-29 11:41:45 +0700

    feat: init project

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import router from './routes/api';
+
+const app = express();
+const port = 4000;
+
+app.use("/api", router)
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
