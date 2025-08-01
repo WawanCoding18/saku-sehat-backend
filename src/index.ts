7b0b29b3afe9a6878bfe865c89d7fad9116c904f
Dzakwan Al iman <dzakwan112016@gmail.com>
2025-08-01 11:21:48 +0700

    check vercel

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,36 @@
-import express from 'express';
-import router from './routes/api';
+import express from "express";
+import router from "./routes/api";
+import db from "./utils/database";
 
-const app = express();
-const port = 4000;
+async function startServer() {
+  try {
+    // Connect to the database
+    const dbConnection = await db();
+    console.log(dbConnection);
 
-app.use("/api", router)
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+    // Create an Express application
+    const app = express();
+
+    // Middleware to parse JSON bodies
+    app.use(express.json());
+
+    app.get("/", (req, res) => {
+      res.status(200).json({
+        message: "Server running",
+        data: null,
+      });
+    });
+    // Use the API routes
+    app.use("/api", router);
+
+    // Start the server
+    const PORT = 4000;
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Error starting the server:", error);
+  }
+}
+
+startServer();
