eb3893c0d4facf88a796cbd7991eaf9f6f343eff
WawanCoding18 <dzakwan112016@gmail.com>
2026-06-24 08:50:43 +0700

    fix vercel.json

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,14 @@ import db from "./utils/database";
 import docs from "./docs/route";
 import cors from "cors";
 
+// Create an Express application
+const app = express();
+
 async function startServer() {
   try {
     // Connect to the database
     const dbConnection = await db();
     console.log(dbConnection);
-
-    // Create an Express application
-    const app = express();
 
     // Middleware to parse JSON bodies and cors
     app.use(cors({ origin: "http://localhost:3000", credentials: true }));
@@ -39,3 +39,4 @@ async function startServer() {
 }
 
 startServer();
+export default app;
