8877fdc2ed5f022915cab96e853197c04650a296
WawanCoding18 <dzakwan112016@gmail.com>
2026-06-25 10:27:23 +0700

    tambah cors

--- src/index.ts
+++ src/index.ts
@@ -91,7 +91,7 @@ import cors from "cors";
 const app = express();
 
 app.use(cors({ 
-  origin: ["http://localhost:3000", "https://front-end-acara-rho-teal.vercel.app"], 
+  origin: ["http://localhost:3000", "https://front-end-event-6bn4.vercel.app"], 
   credentials: true 
 }));
 app.use(express.json());
