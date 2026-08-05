7b843f910c70d8a61ad677eaa53b11cfe0be93f4
aghniyarizky <aghrzky04@gmail.com>
2026-08-05 22:51:37 +0700

    update coba

--- src/middlewares/auth.Middleware.ts
+++ src/middlewares/auth.Middleware.ts
@@ -1,7 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-import { IUserToken } from "../utils/jwt";
-import { generateUser } from "../utils/jwt";
-
+import { IUserToken, generateUser } from "../utils/jwt";
 
 export interface IReqUser extends Request {
   user?: IUserToken;
@@ -9,33 +7,41 @@ export interface IReqUser extends Request {
 
 export default (req: Request, res: Response, next: NextFunction) => {
   const authorization = req.headers?.authorization;
+
   if (!authorization) {
-    return res.status(403).json({
-      message: "unauthorized no token",
+    return res.status(401).json({
+      message: "unauthorized, no token provided",
       data: null,
     });
   }
 
   const [prefix, accessToken] = authorization.split(" ");
 
-  if (!(prefix === "Bearer" && accessToken)) {
-    return res.status(403).json({
-      message: "unauthorized wrong format",
+  if (prefix !== "Bearer" || !accessToken) {
+    return res.status(401).json({
+      message: "unauthorized, wrong format",
       data: null,
     });
   }
 
-//generate user dari access token
-  const user = generateUser(accessToken);
+  // BUNGKUS DENGAN TRY-CATCH DI SINI
+  try {
+    const user = generateUser(accessToken);
 
-  if (!user) {
-    return res.status(403).json({
-      message: "unauthorized invalid user",
+    if (!user) {
+      return res.status(401).json({
+        message: "unauthorized, invalid user",
+        data: null,
+      });
+    }
+
+    (req as IReqUser).user = user;
+    next();
+  } catch (error: any) {
+    // Tangkap error jwt malformed / jwt expired / dsb
+    return res.status(401).json({
+      message: error?.message || "unauthorized, invalid or expired token",
       data: null,
     });
   }
-
-  (req as IReqUser).user = user;
-
-  next();
 };
