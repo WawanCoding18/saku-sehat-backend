eac882dfec10c84abc670795599fc73c45d4e286
WawanCoding18 <dzakwan112016@gmail.com>
2026-08-09 22:51:26 +0700

    fix bug aneh

--- src/models/transaksi.model.ts
+++ src/models/transaksi.model.ts
@@ -1,3 +1,97 @@
+// import mongoose from "mongoose";
+
+// export interface Transaksi {
+//   user: mongoose.Types.ObjectId;
+//   Catatan_Transaksi: string;
+//   tipe: "pengeluaran" | "pemasukan";
+//   kategori: string;
+//   Sumber_Dana: string;
+//   nominal: number;
+//   tanggal: Date;
+//   targetTabungId?: mongoose.Types.ObjectId;
+//   createdAt?: string;
+// }
+
+// const TransaksiSchema = new mongoose.Schema<Transaksi>(
+//   {
+//     user: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: "User",
+//       required: true,
+//     },
+//     Catatan_Transaksi: {
+//       type: String,
+//       required: true,
+//     },
+//     tipe: {
+//       type: String,
+//       enum: ["pengeluaran", "pemasukan"],
+//       required: true,
+//     },
+//     kategori: {
+//       type: String,
+//       enum: [
+//         "Hiburan",
+//         "Makanan",
+//         "Transportasi",
+//         "Belanja",
+//         "Tagihan",
+//         "Kesehatan",
+//         "Gaji",
+//         "Freelance",
+//         "Part-time",
+//         "Investasi",
+//         "Tabungan", 
+//         "Lainnya",
+//       ],
+//       required: true,
+//     },
+//     Sumber_Dana: {
+//       type: String,
+//       enum: [
+//         "Tunai",
+//         "Gopay",
+//         "DANA",
+//         "ShopeePay",
+//         "Bank Mandiri",
+//         "BSI",
+//         "BRI",
+//         "BTN",
+//         "BSA",
+//         "OVO",
+//         "Lainnya",
+//       ],
+//       required: true,
+//     },
+//     nominal: {
+//       type: Number,
+//       required: true,
+//       min: 0,
+//     },
+//     tanggal: {
+//       type: Date,
+//       required: true,
+//     },
+//     targetTabungId: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: "targettabung",
+//       default: null,
+//     },
+//   },
+//   {
+//     timestamps: true,
+//   }
+// );
+
+// const TransaksiModel =
+//   mongoose.models.Transaksi ||
+//   mongoose.model<Transaksi>("Transaksi", TransaksiSchema);
+
+// export default TransaksiModel;
+
+
+
+
 import mongoose from "mongoose";
 
 export interface Transaksi {
@@ -83,6 +177,8 @@ const TransaksiSchema = new mongoose.Schema<Transaksi>(
   }
 );
 
-const TransaksiModel = mongoose.model<Transaksi>("Transaksi", TransaksiSchema);
+const TransaksiModel =
+  mongoose.models.Transaksi ||
+  mongoose.model<Transaksi>("Transaksi", TransaksiSchema);
 
 export default TransaksiModel;
