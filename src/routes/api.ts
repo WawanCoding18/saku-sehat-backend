f382fe9f5c7df4d22e8dcb824c0d5b26c7ef517d
WawanCoding18 <dzakwan112016@gmail.com>
2026-08-09 23:19:04 +0700

    fix bug pinjaman, tabungan, budgete

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -14,7 +14,6 @@ import { getFinancialHealth, postFinancialHealth } from "../controllers/financia
 import { getDashboard } from "../controllers/dashboard.controller";
 import { deleteTargetTabung, editTargetTabung, getAllTargetTabung, postTargetTabung, setorTargetTabung } from "../controllers/targetTabung.controller";
 
-
 const router = express.Router();
 
 //router auth/register, login, and me
@@ -27,15 +26,8 @@ router.post("/auth/resend-otp", authController.resendOTP);
 
 //router profile
 router.post("/profile/onboarding", authMiddleware, postProfile);
-router.get("/profile", authMiddleware, getProfile)
-router.put("/profile", authMiddleware, updateProfile)
-
-//router transaksi
-router.post("/catatan-keuangan/transaksi", authMiddleware, createTransaksi);
-router.get("/catatan-keuangan", authMiddleware, getAllTransaksi);
-router.get("/catatan-keuangan/:id", authMiddleware, getTransaksiById);
-router.put("/catatan-keuangan/:id", authMiddleware, updateTransaksi);
-router.delete("/catatan-keuangan/:id", authMiddleware, deleteTransaksi);
+router.get("/profile", authMiddleware, getProfile);
+router.put("/profile", authMiddleware, updateProfile);
 
 //scan OCR
 router.post('/catatan-keuangan/scan', upload.single('image'), authMiddleware, handleOcrUpload);
@@ -48,21 +40,28 @@ router.put("/catatan-keuangan/pinjaman/:id", authMiddleware, editPinjaman);
 router.delete("/catatan-keuangan/pinjaman/:id", authMiddleware, deletePinjaman);
 
 //router target tabung
-router.post("/catatan-keuangan/target-tabung/tambah-target", authMiddleware, postTargetTabung)
-router.get("/catatan-keuangan/target-tabung", authMiddleware, getAllTargetTabung)
-router.put("/catatan-keuangan/target-tabung/:id", authMiddleware, editTargetTabung)
-router.delete("/catatan-keuangan/target-tabung/:id", authMiddleware, deleteTargetTabung)
-
-//router kalkulator bunga
-router.post('/kalkulator-bunga', authMiddleware, postKalkulatorBunga);
-router.get("/kalkulator-bunga/output", authMiddleware, getKalkulatorBunga); 
+router.post("/catatan-keuangan/target-tabung/tambah-target", authMiddleware, postTargetTabung);
+router.get("/catatan-keuangan/target-tabung", authMiddleware, getAllTargetTabung);
+router.put("/catatan-keuangan/target-tabung/:id/setor", authMiddleware, setorTargetTabung);
+router.put("/catatan-keuangan/target-tabung/:id", authMiddleware, editTargetTabung);
+router.delete("/catatan-keuangan/target-tabung/:id", authMiddleware, deleteTargetTabung);
 
 //router budgeting
 router.post("/catatan-keuangan/budgeting/tambah-budget", authMiddleware, postBudgeting);
 router.get("/catatan-keuangan/budgeting", authMiddleware, getAllBudgeting);
-router.put("/catatan-keuangan/target-tabung/:id/setor", authMiddleware, setorTargetTabung);
 router.put("/catatan-keuangan/budgeting/:id", authMiddleware, updateBudgeting);
 router.delete("/catatan-keuangan/budgeting/:id", authMiddleware, deleteBudgeting);
+
+//router transaksi
+router.post("/catatan-keuangan/transaksi", authMiddleware, createTransaksi);
+router.get("/catatan-keuangan", authMiddleware, getAllTransaksi);
+router.get("/catatan-keuangan/:id", authMiddleware, getTransaksiById);
+router.put("/catatan-keuangan/:id", authMiddleware, updateTransaksi);
+router.delete("/catatan-keuangan/:id", authMiddleware, deleteTransaksi);
+
+//router kalkulator bunga
+router.post('/kalkulator-bunga', authMiddleware, postKalkulatorBunga);
+router.get("/kalkulator-bunga/output", authMiddleware, getKalkulatorBunga); 
 
 //router before you borrow
 router.post("/before-you-borrow", authMiddleware, postBeforeYouBorrow);
@@ -73,10 +72,10 @@ router.post("/cariAman", authMiddleware, postCariAman);
 router.get("/cariAman/output", authMiddleware, getCariAman);
 
 //router financialhealth
-router.post("/financial-health/output", authMiddleware, postFinancialHealth)
-router.get("/dashboard/financial-health", authMiddleware, getFinancialHealth)
+router.post("/financial-health/output", authMiddleware, postFinancialHealth);
+router.get("/dashboard/financial-health", authMiddleware, getFinancialHealth);
 
 //router dashboard
-router.get("/dashboard", authMiddleware, getDashboard)
+router.get("/dashboard", authMiddleware, getDashboard);
 
 export default router;
