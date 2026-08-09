8ae77d8148d4dca1da3cde328e6a123eee100810
WawanCoding18 <dzakwan112016@gmail.com>
2026-08-10 06:02:15 +0700

    fix bug budgeting

--- src/models/budgeting.model.ts
+++ src/models/budgeting.model.ts
@@ -1,6 +1,61 @@
+// import mongoose from "mongoose";
+
+// export interface IBudgeting{
+//   user: mongoose.Types.ObjectId;
+//   Kategori_Budget: string;
+//   Batas_PerBulan: number;
+//   Tanggal_Mulai: Date;
+//   Tanggal_Selesai: Date;
+// }
+
+// const BudgetingSchema = new mongoose.Schema<IBudgeting>(
+//   {
+//     user: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: "User",
+//       required: true,
+//     },
+//     Kategori_Budget: {
+//       type: String,
+//        enum: [
+//         "Hiburan",
+//         "Makanan",
+//         "Transportasi",
+//         "Belanja",
+//         "Tagihan",
+//         "Kesehatan",
+//         "Gaji",
+//         "Freelance",
+//         "Part-time",
+//         "Investasi",
+//         "Lainnya",
+//       ],
+//       required: true,
+//     },
+//     Batas_PerBulan: {
+//       type: Number,
+//       required: true,
+//       min: 0,
+//     },
+//       Tanggal_Mulai: {
+//       type: Date,
+//       required: true,
+//     },
+//       Tanggal_Selesai: {
+//       type: Date,
+//       required: true,
+//     },
+
+//   },
+//   { timestamps: true }
+// );
+
+// const BudgetingModel = mongoose.model<IBudgeting>("budgeting", BudgetingSchema);
+// export default BudgetingModel;
+
 import mongoose from "mongoose";
 
-export interface IBudgeting{
+export interface IBudgeting {
   user: mongoose.Types.ObjectId;
   Kategori_Budget: string;
   Batas_PerBulan: number;
@@ -17,14 +72,17 @@ const BudgetingSchema = new mongoose.Schema<IBudgeting>(
     },
     Kategori_Budget: {
       type: String,
-       enum: [
-        "Hiburan",
-        "Makanan",
+      enum: [
+        "Makanan & Minuman",
         "Transportasi",
         "Belanja",
         "Tagihan",
         "Kesehatan",
+        "Pendidikan",
+        "Hiburan",
+        "Tabungan",
         "Gaji",
+        "Uang Saku",
         "Freelance",
         "Part-time",
         "Investasi",
@@ -37,18 +95,16 @@ const BudgetingSchema = new mongoose.Schema<IBudgeting>(
       required: true,
       min: 0,
     },
-      Tanggal_Mulai: {
+    Tanggal_Mulai: {
       type: Date,
       required: true,
     },
-      Tanggal_Selesai: {
+    Tanggal_Selesai: {
       type: Date,
       required: true,
     },
-
-    
   },
-  { timestamps: true }
+  { timestamps: true },
 );
 
 const BudgetingModel = mongoose.model<IBudgeting>("budgeting", BudgetingSchema);
